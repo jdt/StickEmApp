9d10e60d9406058807a2ef93643d26fca7c7487b
Jesper De Temmerman <jesper@toron.be>
2016-04-03 00:02:46 +0200

    Implementation and test for total calculation on returned amounts

---
+++ Source/StickEmApp/StickEmApp.UnitTest/Entities/AmountReturnedTestFixture.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using StickEmApp.Entities;
+
+namespace StickEmApp.UnitTest.Entities
+{
+    [TestFixture]
+    public class AmountReturnedTestFixture
+    {
+        [Test]
+        public void TestCalculateTotal()
+        {
+            var amount = new AmountReturned
+            {
+                FiveHundreds = 2,       //1000.00
+                TwoHundreds = 3,        // 600.00
+                Hundreds = 4,           // 400.00
+                Fifties = 5,            // 250.00
+                Twenties = 6,           // 120.00
+                Tens = 7,               //  70.00
+                Fives = 8,              //  40.00
+                Twos = 9,               //  18.00
+                Ones = 10,              //  10.00
+                FiftyCents = 11,        //   5.50
+                TwentyCents = 12,       //   2.40
+                TenCents = 13,          //   1.30
+                FiveCents = 14,         //   0.70
+                TwoCents = 15,          //   0.30
+                OneCents = 16           //   0.16
+            };
+
+            Assert.That(amount.CalculateTotal(), Is.EqualTo(new Money(2518.36m)));
+        }
+    }
+}
--- Source/StickEmApp/StickEmApp/Entities/AmountReturned.cs
+++ Source/StickEmApp/StickEmApp/Entities/AmountReturned.cs
@@ -37,5 +37,29 @@
         public virtual int FiveCents { get; set; }
         public virtual int TwoCents { get; set; }
         public virtual int OneCents { get; set; }
+
+        public virtual Money CalculateTotal()
+        {
+            var total = new Money(0);
+
+            total += FiveHundreds * new Money(500);
+            total += TwoHundreds * new Money(200);
+            total += Hundreds * new Money(100);
+            total += Fifties * new Money(50);
+            total += Twenties * new Money(20);
+            total += Tens * new Money(10);
+            total += Fives * new Money(5);
+            total += Twos * new Money(2);
+            total += Ones * new Money(1);
+
+            total += FiftyCents * new Money(0.50m);
+            total += TwentyCents * new Money(0.20m);
+            total += TenCents * new Money(0.10m);
+            total += FiveCents * new Money(0.05m);
+            total += TwoCents * new Money(0.02m);
+            total += OneCents * new Money(0.01m);
+
+            return total;
+        }
     }
 }
