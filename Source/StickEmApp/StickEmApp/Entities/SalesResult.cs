3bf2ec8c0464b9bab163c9cd5b86b25d7f0c661a
Jesper De Temmerman <jesper@toron.be>
2016-04-03 20:52:43 +0200

    Set some default

--- Source/StickEmApp/StickEmApp/Entities/SalesResult.cs
+++ Source/StickEmApp/StickEmApp/Entities/SalesResult.cs
@@ -2,6 +2,12 @@
 {
     public class SalesResult
     {
+        public SalesResult()
+        {
+            Status = ResultType.Exact;
+            Difference = new Money(0);
+        }
+
         public ResultType Status { get; set; }
         public Money Difference { get; set; }
     }
