fca013af4a3bbddb97a275f08156069db6d7b77b
Jesper De Temmerman <jesper@toron.be>
2016-04-06 20:32:34 +0200

    Loading the SummaryViewModel should get and display data

---
+++ Source/StickEmApp/StickEmApp.Windows.UnitTest/ViewModel/SummaryViewModelTestFixture.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using Rhino.Mocks;
+using StickEmApp.Dal;
+using StickEmApp.Entities;
+using StickEmApp.Windows.ViewModel;
+
+namespace StickEmApp.Windows.UnitTest.ViewModel
+{
+    [TestFixture]
+    public class SummaryViewModelTestFixture : UnitOfWorkAwareTestFixture
+    {
+        private IStickerSalesPeriodRepository _stickerSalesPeriodRepository;
+        private StickerSalesPeriod _period;
+
+        private SummaryViewModel _viewModel;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _stickerSalesPeriodRepository = MockRepository.GenerateMock<IStickerSalesPeriodRepository>();
+
+            _period = new StickerSalesPeriod
+            {
+                NumberOfStickersToSell = 55
+            };
+            _stickerSalesPeriodRepository.Expect(repo => repo.Get()).Return(_period);
+
+            _viewModel = new SummaryViewModel(_stickerSalesPeriodRepository);
+        }
+
+        [Test]
+        public void LoadShouldSetNumberOfStickersToSell()
+        {
+            Assert.That(_viewModel.NumberOfStickersToSell, Is.EqualTo(55));
+        }
+    }
+}
--- Source/StickEmApp/StickEmApp.Windows/ViewModel/SummaryViewModel.cs
+++ Source/StickEmApp/StickEmApp.Windows/ViewModel/SummaryViewModel.cs
@@ -1,11 +1,34 @@
 ﻿using System.ComponentModel.Composition;
 using Prism.Mvvm;
+using StickEmApp.Dal;
 
 namespace StickEmApp.Windows.ViewModel
 {
     [Export(typeof(SummaryViewModel))]
     public class SummaryViewModel : BindableBase
     {
-         
+        private int _numberOfStickersToSell;
+
+        [ImportingConstructor]
+        public SummaryViewModel(IStickerSalesPeriodRepository stickerSalesPeriodRepository)
+        {
+            using (new UnitOfWork())
+            {
+                NumberOfStickersToSell = stickerSalesPeriodRepository.Get().NumberOfStickersToSell;
+            }
+        }
+
+        public int NumberOfStickersToSell
+        {
+            get
+            {
+                return _numberOfStickersToSell;
+            }
+            set
+            {
+                _numberOfStickersToSell = value;
+                OnPropertyChanged();
+            }
+        }
     }
 }
