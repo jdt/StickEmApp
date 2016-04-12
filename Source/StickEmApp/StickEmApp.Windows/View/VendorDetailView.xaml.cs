cb446f8e8def312d54269ccbc32fa862e00dd69c
Jesper De Temmerman <jesper@toron.be>
2016-04-12 19:21:42 +0200

    Automatically select text on focus

--- Source/StickEmApp/StickEmApp.Windows/View/VendorDetailView.xaml.cs
+++ Source/StickEmApp/StickEmApp.Windows/View/VendorDetailView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.Composition;
 using System.Windows.Controls;
+using System.Windows.Input;
 using StickEmApp.Windows.Infrastructure;
 using StickEmApp.Windows.Infrastructure.Behaviors;
 using StickEmApp.Windows.ViewModel;
@@ -22,6 +23,21 @@ namespace StickEmApp.Windows.View
         public VendorDetailViewModel ViewModel
         {
             set { DataContext = value; }
+        }
+
+        private void NumericTextBoxKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            SelectAll(sender as TextBox);
+        }
+
+        private void NumericTextBoxMouseFocus(object sender, MouseEventArgs e)
+        {
+            SelectAll(sender as TextBox);
+        }
+
+        private static void SelectAll(TextBox textBox)
+        {
+            textBox.SelectAll();
         }
     }
 }
