e3bf0a2ec08be0802a0da1f8134cb476167249f0
Jesper De Temmerman <jesper@toron.be>
2016-04-14 08:42:57 +0200

    Interface for export to Excel

---
+++ Source/StickEmApp/StickEmApp/Service/IExcelExporter.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using StickEmApp.Entities;
+
+namespace StickEmApp.Service
+{
+    public interface IExcelExporter
+    {
+        void Export(IReadOnlyCollection<Vendor> vendors, string destination);
+    }
+}
