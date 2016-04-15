a907f08e390dc5c4919572cd7b77bbb001ea4b96
Jesper De Temmerman <jesper@toron.be>
2016-04-15 18:42:20 +0200

    Empty implementation of the exporter

---
+++ Source/StickEmApp/StickEmApp/Service/ExcelExporter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using StickEmApp.Entities;
+
+namespace StickEmApp.Service
+{
+    [Export(typeof(IExcelExporter))]
+    public class ExcelExporter : IExcelExporter
+    {
+        public void Export(IReadOnlyCollection<Vendor> vendors, string destination)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
