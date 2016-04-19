0ea178d82ec2b139abb97508abe31052c7273966
Jesper De Temmerman <jesper@toron.be>
2016-04-19 21:11:33 +0200

    Created a resourcemanager that works with Wpf

---
+++ Source/StickEmApp/StickEmApp.Windows/WpfResourceManager.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.Composition;
+using System.Windows;
+using StickEmApp.Service;
+
+namespace StickEmApp.Windows
+{
+    [Export(typeof(IResourceManager))]
+    public class WpfResourceManager : IResourceManager
+    {
+        public string GetString(string resourceName)
+        {
+            return Application.Current.Resources[resourceName].ToString();
+        }
+    }
+}
