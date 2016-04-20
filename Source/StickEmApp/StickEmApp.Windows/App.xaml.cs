f16f867932a9c3e7aa058a8bbefc4501d9a02178
Jesper De Temmerman <jesper@toron.be>
2016-04-20 21:45:19 +0200

    Added logging of exceptions

--- Source/StickEmApp/StickEmApp.Windows/App.xaml.cs
+++ Source/StickEmApp/StickEmApp.Windows/App.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Globalization;
 using System.Threading;
 using System.Windows;
 using System.Windows.Markup;
+using System.Windows.Threading;
+using log4net;
 
 namespace StickEmApp.Windows
 {
@@ -10,9 +13,18 @@ namespace StickEmApp.Windows
     /// </summary>
     public partial class App : Application
     {
+        private static readonly ILog Logger = LogManager.GetLogger("StickEmApp");
+
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
+
+            log4net.Config.XmlConfigurator.Configure();
+
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
+            if(Logger.IsInfoEnabled)
+                Logger.InfoFormat("Application started at {0}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
 
             Thread.CurrentThread.CurrentCulture = new CultureInfo("nl-BE");
             Thread.CurrentThread.CurrentUICulture = new CultureInfo("nl-BE");
@@ -20,6 +32,16 @@ namespace StickEmApp.Windows
             
             var bootstrapper = new StickEmAppBootStrapper();
             bootstrapper.Run();
+        }
+
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Logger.Error("Unhandled exception on the AppDomain.CurrentDomain", e.ExceptionObject as Exception);
+        }
+
+        private void App_OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            Logger.Error("Unhandled Dispatcher exception", e.Exception);
         }
     }
 }
